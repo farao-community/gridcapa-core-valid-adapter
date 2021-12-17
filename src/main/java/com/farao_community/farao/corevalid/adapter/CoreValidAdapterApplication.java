62320fbff51777fbc5cb563ee31febe97ad3f5ed
Montigny <alexandre.montigny@rte-france.com>
2021-12-17 12:11:59 +0100

    Spring initializr commit

---
+++ src/main/java/com/farao_community/farao/corevalid/adapter/CoreValidAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.farao_community.farao.corevalid.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreValidAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreValidAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farao_community/farao/corevalid/adapter/CoreValidAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farao_community.farao.corevalid.adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreValidAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
